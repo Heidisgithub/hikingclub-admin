8e5e14cb7fc3ce3016a33c36a8ba867acd26b816
Radu Baltag <baltag.radu@gmail.com>
2021-10-14 18:52:40 +0200

    basename for router

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/hikingclub-admin">
       <App />
   </BrowserRouter>,
   document.getElementById('root')
