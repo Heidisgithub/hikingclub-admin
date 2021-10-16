2017af771be8a985d63dca651943ce2638a1dc40
Radu Baltag <baltag.radu@gmail.com>
2021-10-16 10:03:36 +0200

    modal closing functionality

--- src/Modal.js
+++ src/Modal.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 
 function Modal(props) {
     const showHideClassName = props.show ? "modal display-block" : "modal display-none";
 
+    const domNode = useRef();
+
+    const handleKey = e => {
+        if (e.code === 'Escape') {
+            props.handleClose()
+        }
+    }
+
+    const handleClick = e => {
+        if (!domNode.current.contains(e.target)) {
+            props.handleClose()
+        }
+    }
+
+    useEffect(
+        () => {
+            document.addEventListener('keydown', handleKey)
+            document.addEventListener('mousedown', handleClick)
+            return () => {
+                document.removeEventListener('keydown', handleKey)
+                document.removeEventListener('mousedown', handleClick)
+            }
+        }, []
+    )
+
     return (
         <div className={showHideClassName}>
-            <section className="modal-main">
+            <section ref={domNode} className="modal-main">
                 {props.children}
                 <button type="button" onClick={props.handleClose}>
                     Close
