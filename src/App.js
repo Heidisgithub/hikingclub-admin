c25604c8241acd5c53e667bb97576d52d0ac1b82
Erik <elaktrato@gmail.com>
2021-11-13 17:08:41 +0100

    Login and validation on registrations

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
 import './App.css';
 import { NavLink } from "react-router-dom";
-import EntityList from "./EntityList"
-import {useEffect} from 'react'
+import EntityList from "./EntityList";
+import {useState} from "react";
+import { useEffect } from 'react/cjs/react.development';
 
-async function login(email, password) {
-  let jsonResponse = { error: "unknown" };
-  const url = `${process.env.REACT_APP_DEV_URL_HOMEPAGE}/login`
-  const myInit = {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({email, password})
-  }
-  const response = await fetch(url, myInit)
-  if (response.ok) {
-    jsonResponse = await response.json()
-    localStorage.setItem("sessionId", jsonResponse.sessionId)
-  } else {
-    console.log("login unsuccessful")
-  }
-}
+
 
 function App() {
-  useEffect(()=>{
-    login(process.env.REACT_APP_DEV_ADMIN_EMAIL, process.env.REACT_APP_DEV_ADMIN_PW)
-  }, [])
+
+  const[loggedIn, setLoggedIn] = useState(false)
+
+  async function login(email, password) {
+    console.log(email)
+    console.log(password)
+  
+    let jsonResponse = { error: "unknown" };
+    const url = `${process.env.REACT_APP_DEV_URL_HOMEPAGE}/login`
+    const myInit = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({email, password})
+    }
+    const response = await fetch(url, myInit)
+    if (response.ok) {
+      jsonResponse = await response.json()
+      localStorage.setItem("sessionId", jsonResponse.sessionId)
+      setLoggedIn(true)
+    } else {
+      console.log("login unsuccessful")
+      logOut();
+    }
+  }
+
+  
+
+useEffect(() => {
+  if(localStorage.getItem('sessionId')){
+    setLoggedIn(true)
+  }
+  
+}, [])
 
   const envUrl = () => {
     if (process.env.NODE_ENV === 'development') {
@@ -32,13 +48,18 @@ function App() {
       return 'https://wbshikingclub.herokuapp.com/'
     }
   }
+
+  const logOut = () => {
+    localStorage.clear(); setLoggedIn(false)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         
         <nav className="NavButtons">
           <div className="logout">
-            <a href={envUrl()}>Log out</a>
+            <button onClick={logOut}>Log out</button>
           </div>
           <NavLink
           to="/hikes"
@@ -63,7 +84,7 @@ function App() {
         </nav>
       </header>
 
-      <EntityList />
+      <EntityList login={login} loggedIn={loggedIn} logOut={logOut} />
     </div>
   );
 }
